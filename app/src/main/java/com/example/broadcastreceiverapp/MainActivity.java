181d9e4b8350b85cd88ca9786402e141b8a59523
Oswaldo26 <ozanca@unsa.edu.pe>
2022-10-31 12:47:49 -0500

    Initial commit Receiver

---
+++ app/src/main/java/com/example/broadcastreceiverapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.broadcastreceiverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IntentFilter intentFilter = new IntentFilter("com.prueba.miMensajeBroadcast");
+        MyBroadcastReceiver objReceiver = new MyBroadcastReceiver();
+        registerReceiver(objReceiver,intentFilter);
+    }
+}
